f92ac33e0fb6074d58adda1309dcc9781cd5145d
hamzaja <hamzajaved71@gmail.com>
2019-09-10 16:26:04 -0400

    message form

--- src/Messages.js
+++ src/Messages.js
@@ -2,11 +2,32 @@ import React from 'react';
 import MessageForm from './MessageForm'
 class Messages extends React.Component{
 
+state={
+  messages: []
+}
+
+componentDidUpdate(){
+  let abc ;
+  if (abc !== this.props.chatPerson.id){
+    fetch("http://localhost:3000/chat_boxes")
+    .then(res=> res.json())
+    .then(data =>{
+      data.map(message => {
+        if(message.user_id === parseInt(localStorage.id) && this.props.chatid === message.chat_id){
+          this.setState({messages : [...this.state.messages , message ] })
+          abc = this.props.chatPerson.id
+        }
+      })
+    })
+  }
+}
 
 render(){
+  console.log(this.state.message)
     return (
-      <div>
-        {this.props.chatPerson.name}
+      <div >
+        {this.props.chatPerson.id}
+
         <MessageForm chatid = {this.props.chatid}/>
       </div>
     )
